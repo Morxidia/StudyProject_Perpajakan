b0b8bb8ef52c3748980f57507cb312e781c96d0a
Rai710 <raihanbuono@gmail.com>
2025-05-15 18:07:33 +0700

    addmin

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,19 @@
+string getPassword() {
+//     string password;
+//     char ch;
+
+//     while (true) {
+//         ch = _getch(); 
+
+//         if (ch == 13) break; // Enter
+//         else if (ch == 8 && !password.empty()) { // Backspace
+//             password.pop_back();
+//             cout << "\b \b"; 
+//         } else if (isprint(ch)) {
+//             password.push_back(ch);
+//             cout << '*'; 
+//     }
+
+//     return password ;
+//     }
+// }
